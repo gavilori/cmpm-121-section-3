aaafc617d05227628b07e36510c06107318258ec
ofcourseitsgv <84610338+ofcourseitsgv@users.noreply.github.com>
2023-10-24 18:47:41 -0700

    finish solo dev

--- src/scenes/Play.ts
+++ src/scenes/Play.ts
@@ -2,15 +2,28 @@ import * as Phaser from "phaser";
 
 import starfieldUrl from "/assets/starfield.png";
 
+/*
+class Enemy {
+  moveSpeed?: number;
+
+  constructor(speed: number) {
+    this.moveSpeed = speed;
+  }
+
+  update() {}
+}
+*/
+
 export default class Play extends Phaser.Scene {
   fire?: Phaser.Input.Keyboard.Key;
   left?: Phaser.Input.Keyboard.Key;
   right?: Phaser.Input.Keyboard.Key;
 
   starfield?: Phaser.GameObjects.TileSprite;
-  spinner?: Phaser.GameObjects.Shape;
+  player?: Phaser.GameObjects.Shape;
 
   rotationSpeed = Phaser.Math.PI2 / 1000; // radians per millisecond
+  movementSpeed = 3;
 
   constructor() {
     super("play");
@@ -41,26 +54,49 @@ export default class Play extends Phaser.Scene {
       )
       .setOrigin(0, 0);
 
-    this.spinner = this.add.rectangle(100, 100, 50, 50, 0xff0000);
+    this.player = this.add.rectangle(
+      (this.game.config.width as number) / 2,
+      (this.game.config.height as number) - 50,
+      25,
+      25,
+      0xff0000,
+    );
+
+    // this.ship1 = new Enemy();
   }
+
+  isLaunched = false;
 
   update(_timeMs: number, delta: number) {
     this.starfield!.tilePositionX -= 4;
 
-    if (this.left!.isDown) {
-      this.spinner!.rotation -= delta * this.rotationSpeed;
+    if (this.left!.isDown && !this.isLaunched) {
+      this.player!.rotation -= delta * this.rotationSpeed;
+      this.player!.x -= this.movementSpeed;
     }
-    if (this.right!.isDown) {
-      this.spinner!.rotation += delta * this.rotationSpeed;
+    if (this.right!.isDown && !this.isLaunched) {
+      this.player!.rotation += delta * this.rotationSpeed;
+      this.player!.x += this.movementSpeed;
     }
 
-    if (this.fire!.isDown) {
+    if (this.fire!.isDown && !this.isLaunched) {
       this.tweens.add({
-        targets: this.spinner,
+        targets: this.player,
         scale: { from: 1.5, to: 1 },
         duration: 300,
         ease: Phaser.Math.Easing.Sine.Out,
       });
+
+      this.isLaunched = true;
+    }
+
+    if (this.isLaunched) {
+      this.player!.y -= this.movementSpeed;
+    }
+
+    if (this.player!.y < 0) {
+      this.player!.y = (this.game.config.height as number) - 50;
+      this.isLaunched = false;
     }
   }
 }
